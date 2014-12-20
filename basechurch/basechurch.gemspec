bb12036109f676b1361e7e895261a46e79c6ce65
Amos Chan <amosschan@gmail.com>
2014-12-20 15:53:03 -0500

    Move model specs to engine

--- basechurch/basechurch.gemspec
+++ basechurch/basechurch.gemspec
@@ -16,7 +16,5 @@ Gem::Specification.new do |s|
 
   s.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.rdoc"]
 
-  s.add_dependency "rails", "~> 4.1.7"
-
-  s.add_development_dependency "sqlite3"
+  s.add_dependency "rspec-rails"
 end
--- basechurch/lib/basechurch/engine.rb
+++ basechurch/lib/basechurch/engine.rb
@@ -3,8 +3,16 @@ module Basechurch
     isolate_namespace Basechurch
 
     config.generators do |g|
-      g.test_framework :rspec
+      g.test_framework :rspec, view_specs: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
     end
   end
 end
