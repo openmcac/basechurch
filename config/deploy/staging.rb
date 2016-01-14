83dfa278e935e3c09b1af22ad55ad5d2dc46d2a4
Amos Chan <amosschan@gmail.com>
2016-01-13 22:58:29 -0500

    Update staging ip

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
-server "159.203.38.244", user: "deploy", roles: %w{web app db}
+server "159.203.10.73", user: "deploy", roles: %w{web app db}
 set :deploy_to, '/u/apps/mcac_staging'
 set :unicorn_config_path, "/u/apps/mcac_staging/current/config/unicorn.rb"
