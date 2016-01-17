226816fc96507db610966263efd2cd81c6e01657
Amos Chan <amosschan@gmail.com>
2016-01-16 23:46:17 -0500

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,82 +5,82 @@ user = User.create(email: 'test@example.com',
                    password: 'password',
                    provider: "email")
 
-return if Rails.env.production?
+unless Rails.env.production?
+  service_order =
+    " - ## Call to Worship
+   - ## Praise & Worship
+   - ## Announcements
+   - ## Offering
+   - ## Holy Communion
+   - ## Sermon
+     \"We Will Love The World\"  
+     Genesis 2:4-15
+   - ## Doxology
+   - ## Benediction"
 
-service_order =
-  " - ## Call to Worship
- - ## Praise & Worship
- - ## Announcements
- - ## Offering
- - ## Holy Communion
- - ## Sermon
-   \"We Will Love The World\"  
-   Genesis 2:4-15
- - ## Doxology
- - ## Benediction"
+  bulletin = Bulletin.create(published_at: DateTime.new(2010, 01, 3),
+                             group: group,
+                             name: 'Holy Communion New Year Sunday Service',
+                             description: 'January 3, 2010 at 9:30 am',
+                             service_order: service_order)
+  post = Post.create(author: user,
+                     group: group,
+                     title: 'This is a title',
+                     content: 'This is my post content',
+                     published_at: DateTime.now)
 
-bulletin = Bulletin.create(published_at: DateTime.new(2010, 01, 3),
-                           group: group,
-                           name: 'Holy Communion New Year Sunday Service',
-                           description: 'January 3, 2010 at 9:30 am',
-                           service_order: service_order)
-post = Post.create(author: user,
-                   group: group,
-                   title: 'This is a title',
-                   content: 'This is my post content',
-                   published_at: DateTime.now)
-
-[
-  "**Welcome friends and visitors.** We invite you to make MCAC your spiritual
-home. Please let us know how we can be of help to you.",
-  "The **2016 Church theme** is \"Equip to Shepherd\". *\"..to equip his people
-for works of servce, so that the body of Christ may be built up\" (Ephesians
-4:12)",
-  "We have received **162 pledge cards**, pledging an amount of **$103,510**.
-If you have participated in the pledge, when you offer please specify
-\"Missions\". Also remember to put 2016 as the year if you offer with checks.",
-  "Praise the Lord for the opportunities to reach out to our community through
-the Christmas activities: the Celebrations in our 3 congregations, caroling in
-an elderly home and with CCM's Christmas gifts to elderly and homeless... May
-more people learn about the true meaning and the reason of Christmas.",
-  "Rev. Chan and Auntie Cindy have left for Hong Kong on Wednesday to visit
-family and friends. They will return on the 21st of January.",
-  "There will be a **Jacob Fellowship** prayer meeting on Sunday, January 10
-from 11:30am to 12:30pm in the library.",
-  "Sunday school classes will resume next Sunday.",
-  "There will be an Elders Board meeting on January 24th. 2016 ministry annual
-report due the end of January. Annual Membership meeting takes place on
-February 28th 11:30am after the joint service at 10am.",
-  "**2016 Church Directory** is now available. Ask ushers for a copy. 1 per
-family.",
-  "**Church Renewal Weekend** offered by the Southland Church in Steinback, MB.
-January 29 - February 1, 2016. Main speaker: Pastor Ray Duerksen. Fee for the
-sessions: $139/person. Session and events include: Retreats, Ministry Tours,
-Worships on Church Renewal and Hearing God, Prayer Summit and fellowships. For
-more info, visit
-[churchrenewalministry.com](http://www.churchrenewalministry.com)."
-].each_with_index do |a, i|
-  Announcement.create(post: post,
-                      bulletin: bulletin,
-                      description: a,
-                      position: i + 1)
-end
-
-100.times do
-  service_order = ""
-  (5 + Random.rand(5)).times do
-    service_order = "#{service_order}\n- ## #{ Forgery('lorem_ipsum').word(random: true).capitalize } #{ Forgery('lorem_ipsum').words(Random.rand(3), random: true) }"
+  [
+    "**Welcome friends and visitors.** We invite you to make MCAC your spiritual
+  home. Please let us know how we can be of help to you.",
+    "The **2016 Church theme** is \"Equip to Shepherd\". *\"..to equip his people
+  for works of servce, so that the body of Christ may be built up\" (Ephesians
+  4:12)",
+    "We have received **162 pledge cards**, pledging an amount of **$103,510**.
+  If you have participated in the pledge, when you offer please specify
+  \"Missions\". Also remember to put 2016 as the year if you offer with checks.",
+    "Praise the Lord for the opportunities to reach out to our community through
+  the Christmas activities: the Celebrations in our 3 congregations, caroling in
+  an elderly home and with CCM's Christmas gifts to elderly and homeless... May
+  more people learn about the true meaning and the reason of Christmas.",
+    "Rev. Chan and Auntie Cindy have left for Hong Kong on Wednesday to visit
+  family and friends. They will return on the 21st of January.",
+    "There will be a **Jacob Fellowship** prayer meeting on Sunday, January 10
+  from 11:30am to 12:30pm in the library.",
+    "Sunday school classes will resume next Sunday.",
+    "There will be an Elders Board meeting on January 24th. 2016 ministry annual
+  report due the end of January. Annual Membership meeting takes place on
+  February 28th 11:30am after the joint service at 10am.",
+    "**2016 Church Directory** is now available. Ask ushers for a copy. 1 per
+  family.",
+    "**Church Renewal Weekend** offered by the Southland Church in Steinback, MB.
+  January 29 - February 1, 2016. Main speaker: Pastor Ray Duerksen. Fee for the
+  sessions: $139/person. Session and events include: Retreats, Ministry Tours,
+  Worships on Church Renewal and Hearing God, Prayer Summit and fellowships. For
+  more info, visit
+  [churchrenewalministry.com](http://www.churchrenewalministry.com)."
+  ].each_with_index do |a, i|
+    Announcement.create(post: post,
+                        bulletin: bulletin,
+                        description: a,
+                        position: i + 1)
   end
 
-  bulletin =
-    Bulletin.create(published_at: Random.rand(1000).days.ago,
-                    group: group,
-                    name: Forgery('lorem_ipsum').title(random: true),
-                    description: "#{ Forgery('lorem_ipsum').word(random: true).capitalize } #{ Forgery('lorem_ipsum').words(Random.rand(5), random: true) }",
-                    service_order: service_order,
-                    sermon_notes: Forgery('email').body(random: true))
+  100.times do
+    service_order = ""
+    (5 + Random.rand(5)).times do
+      service_order = "#{service_order}\n- ## #{ Forgery('lorem_ipsum').word(random: true).capitalize } #{ Forgery('lorem_ipsum').words(Random.rand(3), random: true) }"
+    end
 
-  (6 + Random.rand(5)).times do
-    bulletin.announcements.build(description: Forgery('lorem_ipsum').sentences(Random.rand(7), random: true)).save
+    bulletin =
+      Bulletin.create(published_at: Random.rand(1000).days.ago,
+                      group: group,
+                      name: Forgery('lorem_ipsum').title(random: true),
+                      description: "#{ Forgery('lorem_ipsum').word(random: true).capitalize } #{ Forgery('lorem_ipsum').words(Random.rand(5), random: true) }",
+                      service_order: service_order,
+                      sermon_notes: Forgery('email').body(random: true))
+
+    (6 + Random.rand(5)).times do
+      bulletin.announcements.build(description: Forgery('lorem_ipsum').sentences(Random.rand(7), random: true)).save
+    end
   end
 end
