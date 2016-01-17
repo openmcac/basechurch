a5cc29db63853c896b0092bb89967409f3d2e02c
Amos Chan <amosschan@gmail.com>
2016-01-17 12:44:03 -0500

    Fetch bulletin groups while unauthenticated

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::GroupsController < ApplicationResourceController
-  before_action :authenticate_user!, except: [:index, :show, :get_related_resources]
+  before_action :authenticate_user!, except: [
+    :index,
+    :show,
+    :get_related_resources,
+    :get_related_resource
+  ]
 
   def sign
     render json: S3Signer.new.sign(type: params[:type], directory: "groups"),
