a8b7a918d16991860456989b28696fcf0f9ca0be
Amos Chan <amosschan@gmail.com>
2016-02-07 22:53:14 -0500

    Add pagination support

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -1,4 +1,5 @@
 JSONAPI.configure do |config|
   # built in key format options are :underscored_key, :camelized_key and :dasherized_key
   config.json_key_format = :dasherized_key
+  config.default_paginator = :paged
 end
