f5065710a4708fce41da850f42673fa9239a3363
Amos Chan <amosschan@gmail.com>
2016-04-15 17:11:52 -0400

    Update GroupResource to include new group fields

--- spec/resources/api/v1/group_resource_spec.rb
+++ spec/resources/api/v1/group_resource_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::GroupResource, type: :resource do
-  let(:group) { create(:group) }
+  let(:group) do
+    create(:group,
+           banner_url: "http://#{Forgery("internet").domain_name}/banner.jpg",
+           profile_picture_url: "http://#{Forgery("internet").domain_name}/profile.jpg")
+  end
   let(:records) { Group.all }
 
   subject { Api::V1::GroupResource.new(group, nil) }
@@ -11,7 +15,11 @@ RSpec.describe Api::V1::GroupResource, type: :resource do
   its(:slug) { is_expected.to eq(group.slug) }
   its(:created_at) { is_expected.to eq(group.created_at) }
   its(:about) { is_expected.to eq(group.about) }
+  its(:short_description) { is_expected.to eq(group.short_description) }
+  its(:target_audience) { is_expected.to eq(group.target_audience) }
+  its(:meet_details) { is_expected.to eq(group.meet_details) }
   its(:banner_url) { is_expected.to eq(group.banner_url) }
+  its(:profile_picture_url) { is_expected.to eq(group.profile_picture_url) }
 
   describe 'apply_filter' do
     let(:options) { {} }
