152afd921c44848dbb75264b3081affa4ab1393f
Amos Chan <amosschan@gmail.com>
2016-08-08 18:38:18 -0400

    Remove old deployment script

--- config/deploy/production.rb
+++
@@ -1,4 +0,0 @@
-server "159.203.115.125", user: "deploy", roles: %w{web app db}
-set :deploy_to, '/u/apps/mcac_production'
-set :unicorn_config_path, "/u/apps/mcac_production/current/config/unicorn.rb"
-
--- config/deploy/staging.rb
+++
@@ -1,3 +0,0 @@
-server "45.55.126.192", user: "deploy", roles: %w{web app db}
-set :deploy_to, '/u/apps/mcac_staging'
-set :unicorn_config_path, "/u/apps/mcac_staging/current/config/unicorn.rb"
