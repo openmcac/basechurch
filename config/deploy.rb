6c317bc1e61ee1648a12ccb7bdf2fab68b42f1ae
Amos Chan <amosschan@gmail.com>
2016-08-17 00:05:31 -0400

    Migrate, deploy and restart puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,8 @@
 # Change these
-server '45.55.126.192', port: 3000, roles: %w{app web}, primary: true
+stage = "staging"
+server '45.55.126.192', port: 22, roles: %w{app web}, primary: true
 
-set :repo_url,        'git@github.com:openmcac/basechurch.git'
+set :repo_url,        'https://github.com/openmcac/basechurch.git'
 set :application,     'mcac'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
@@ -10,28 +11,32 @@ set :puma_workers,    2
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
-set :stage,           :production
+set :stage,           stage
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_env,        stage
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :default_env, { path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH" }
+
+branch = ENV.fetch("BRANCH", "master")
+set :branch,          branch
 
 ## Defaults:
 # set :scm,           :git
-# set :branch,        :master
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml, config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
@@ -50,8 +55,8 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
+      unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
+        puts "WARNING: HEAD is not the same as origin/#{branch}"
         puts "Run `git push` to sync changes."
         exit
       end
