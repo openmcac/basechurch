564d271084a2734c846a2c6150f4a84f340bde0d
Amos Chan <amosschan@gmail.com>
2016-10-10 17:49:20 -0400

    Revert to minimal sermon factory

--- spec/support/factories/sermons.rb
+++ spec/support/factories/sermons.rb
@@ -4,13 +4,12 @@ FactoryGirl.define do
     name { Forgery('lorem_ipsum').sentence }
     published_at { DateTime.now }
     speaker { Forgery('name').full_name }
-    audio_url { "http://#{Forgery('internet').domain_name}/audio.mp3" }
-    banner_url { "http://#{Forgery('internet').domain_name}/banner.jpg" }
 
     trait :completed do
       notes { Forgery(:lorem_ipsum).words(10) }
-      speaker { Forgery('name').full_name }
       series { Forgery('lorem_ipsum').title }
+      audio_url { "http://#{Forgery('internet').domain_name}/audio.mp3" }
+      banner_url { "http://#{Forgery('internet').domain_name}/banner.jpg" }
     end
   end
 end
