d03438e0dbe455fc15b3eb012b474c69ec243469
Amos Chan <amosschan@gmail.com>
2016-10-10 17:51:50 -0400

    Remove unused shared examples

--- spec/support/shared_examples/authenticated_action.rb
+++ spec/support/shared_examples/authenticated_action.rb
@@ -1,16 +1,3 @@
-# required variables:
-#  - perform_action
-shared_examples_for 'an authenticated action' do
-  before do
-    request.headers['Content-Type'] = 'application/vnd.api+json'
-    perform_action
-  end
-
-  it 'returns a 401 status code' do
-    expect(response.status).to eq(401)
-  end
-end
-
 def example_authenticated_request(example_name, request_options = nil, &block)
   before do
     header "Content-Type", "application/vnd.api+json"
--- spec/support/shared_examples/s3_signing.rb
+++ spec/support/shared_examples/s3_signing.rb
@@ -1,38 +1,5 @@
 # variables
 #  - directory
-shared_examples_for "a request that returns a signature to upload to s3" do
-  describe "#sign" do
-    context "when unauthenticated" do
-      before do
-        get :sign, name: "myfile.jpg", size: 12343, type: "image/jpeg"
-      end
-
-      it "requires authentication" do
-        expect(response.status).to eq(401)
-      end
-    end
-
-    context "with a signed in user" do
-      let(:signed_hash) { { "random" => "hash" } }
-
-      before do
-        auth_headers = user.create_new_auth_token
-        @request.headers.merge!(auth_headers)
-
-        allow_any_instance_of(S3Signer).to receive(:sign).
-          with(type: "image/jpeg", directory: directory).
-          and_return(signed_hash)
-
-        get :sign, name: "myfile.jpg", size: 12343, type: "image/jpeg"
-      end
-
-      it "responds with values to post to s3" do
-        expect(JSON.parse(response.body)).to eq signed_hash
-      end
-    end
-  end
-end
-
 shared_examples_for "an endpoint that returns a signature to upload to s3" do
   parameter :name, "Attachment filename"
   parameter :size, "Attachment filesize in kb"
