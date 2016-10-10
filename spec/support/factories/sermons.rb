0203ac06243b13587ef993740f228f104633c17d
Amos Chan <amosschan@gmail.com>
2016-10-10 19:13:35 -0400

    Add specs for sermon tagging

--- spec/support/factories/sermons.rb
+++ spec/support/factories/sermons.rb
@@ -6,10 +6,11 @@ FactoryGirl.define do
     speaker { Forgery('name').full_name }
 
     trait :completed do
-      notes { Forgery(:lorem_ipsum).words(10) }
-      series { Forgery('lorem_ipsum').title }
       audio_url { "http://#{Forgery('internet').domain_name}/audio.mp3" }
       banner_url { "http://#{Forgery('internet').domain_name}/banner.jpg" }
+      notes { Forgery(:lorem_ipsum).words(10) }
+      series { Forgery('lorem_ipsum').title }
+      tag_list { Forgery("lorem_ipsum").lorem_ipsum_words.sample(3) }
     end
   end
 end
