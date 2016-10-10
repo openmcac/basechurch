b68a3a0e47b193470ef88fbeeaf646105f51b6cc
Amos Chan <amosschan@gmail.com>
2016-10-10 14:40:57 -0400

    Indicate unused block parameter

--- config/initializers/rspec_api_documentation.rb
+++ config/initializers/rspec_api_documentation.rb
@@ -1,6 +1,7 @@
 RspecApiDocumentation.configure do |config|
   config.post_body_formatter = :json
-  config.response_body_formatter = Proc.new do |response_content_type, response_body|
-    JSON.pretty_generate(JSON.parse(response_body))
-  end
+  config.response_body_formatter =
+    Proc.new do |_response_content_type, response_body|
+      JSON.pretty_generate(JSON.parse(response_body))
+    end
 end
