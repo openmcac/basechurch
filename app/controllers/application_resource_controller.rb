3654c7e9ab4568cbbb0557208ce8c4aca699ecf0
Amos Chan <amosschan@gmail.com>
2016-10-10 23:56:10 -0400

    Remove CORS headers

--- app/controllers/application_resource_controller.rb
+++ app/controllers/application_resource_controller.rb
@@ -4,8 +4,6 @@ class ApplicationResourceController < JSONAPI::ResourceController
   include ActionController::StrongParameters
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  before_action :set_cors_headers, if: "Rails.env.development?"
-
   private
 
   def context
@@ -16,14 +14,5 @@ class ApplicationResourceController < JSONAPI::ResourceController
 
   def resource_serializer_klass
     @resource_serializer_klass ||= BasechurchResourceSerializer
-  end
-
-  def set_cors_headers
-    headers["Access-Control-Allow-Origin"] = "*"
-    headers["Access-Control-Allow-Headers"] =
-      "X-AUTH-TOKEN, X-API-VERSION, X-Requested-With, Content-Type, Accept, Origin"
-    headers["Access-Control-Allow-Methods"] =
-      "POST, GET, PUT, DELETE, OPTIONS"
-    headers["Access-Control-Max-Age"] = "1728000"
   end
 end
