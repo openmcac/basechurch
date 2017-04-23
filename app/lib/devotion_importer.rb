4b6ff7d3094b2ff5da1d602d256d952ec13a9179
Amos Chan <amosschan@gmail.com>
2017-04-23 16:13:20 -0400

    Remove hardcoded dates

--- app/lib/devotion_importer.rb
+++ app/lib/devotion_importer.rb
@@ -4,22 +4,19 @@ class DevotionImporter
     devotions = contents.split("-----")
     group = Group.last
     author = User.first
-    last_post_published_at = Post.find(62).published_at
-    last_day = 18
+    first_post = Group.last.posts.first
     devotions.map(&:strip).each do |d|
       post = Post.new
-      current_day = last_day + 1
+      published_at = Post.last.published_at + 24.hours
+      current_day = (published_at.to_datetime - first_post.published_at.to_datetime).to_i + 1
       post.title = "Pastoral Devotions: Day #{current_day}"
       post.author = author
       post.group = group
-      post.published_at = last_post_published_at + 24.hours
+      post.published_at = published_at
       post.content = d
       post.save!
 
       puts "saved day #{current_day}"
-
-      last_post_published_at = post.published_at
-      last_day = current_day
     end
   end
 end
