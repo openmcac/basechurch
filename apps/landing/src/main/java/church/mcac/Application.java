6fc9257299711c5a3be5f252933e2f5c3afbf08b
Amos Chan <amosschan@gmail.com>
2017-09-04 15:17:35 -0400

    Determine application environment

--- apps/landing/src/main/java/church/mcac/Application.java
+++ apps/landing/src/main/java/church/mcac/Application.java
@@ -7,7 +7,11 @@ import static spark.Spark.get;
 public class Application {
   public static void main(String[] args) {
     Jedis jedis = new Jedis(System.getenv().get("REDIS_HOST"));
-    jedis.set("basechurch:index:__development__", "<html><body><h1>Hello world</h1></body></html>");
+
+    Environment environment = Environment.fromEnvironmentVariable(System.getenv("ENVIRONMENT"));
+    if (environment == Environment.DEVELOPMENT) {
+      jedis.set("basechurch:index:__development__", "<html><body><h1>Hello world</h1></body></html>");
+    }
 
     get("/", LandingController.indexRoute(jedis));
   }
---
+++ apps/landing/src/main/java/church/mcac/Environment.java
@@ -0,0 +1,20 @@
+package church.mcac;
+
+public enum Environment {
+  TEST, DEVELOPMENT, STAGING, PRODUCTION;
+
+  public static Environment fromEnvironmentVariable(String envVar) {
+    switch (envVar.toLowerCase()) {
+      case "test":
+        return TEST;
+      case "development":
+        return DEVELOPMENT;
+      case "staging":
+        return STAGING;
+      case "production":
+        return PRODUCTION;
+      default:
+        return DEVELOPMENT;
+    }
+  }
+}
