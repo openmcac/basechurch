4dbc3a4321bd7e00a02aecbf586fe76388b7fe25
Amos Chan <amosschan@gmail.com>
2017-10-10 16:18:33 -0400

    Add landing specs to travis

--- bin/test-basechurch.sh
+++ bin/test-basechurch.sh
@@ -8,14 +8,14 @@ docker-compose \
 docker-compose \
   -f apps/basechurch/docker-compose.yml \
   -f apps/basechurch/docker-compose.override.yml \
-  run -e RAILS_ENV=test web bundle exec rake db:create
+  run -e RAILS_ENV=test api bundle exec rake db:create
 
 docker-compose \
   -f apps/basechurch/docker-compose.yml \
   -f apps/basechurch/docker-compose.override.yml \
-  run -e RAILS_ENV=test web bundle exec rake db:migrate
+  run -e RAILS_ENV=test api bundle exec rake db:migrate
 
 docker-compose \
   -f apps/basechurch/docker-compose.yml \
   -f apps/basechurch/docker-compose.override.yml \
-  run web bundle exec rspec
+  run api bundle exec rspec
