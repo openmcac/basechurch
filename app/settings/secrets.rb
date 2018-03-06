43e7711bbe12af426dc2d211e19976cc473649a8
Amos Chan <amosschan@gmail.com>
2018-03-05 20:55:09 -0500

    Use ENV instead of secrets

--- app/settings/secrets.rb
+++
@@ -1,5 +0,0 @@
-class Secrets < Settingslogic
-  source "#{Rails.root}/config/secrets.yml"
-
-  namespace Rails.env
-end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 ::OmniAuthConfig = Proc.new do
-  provider :facebook, Secrets.fb_app_id, Secrets.fb_app_secret
+  provider :facebook, ENV["fb_app_id"], ENV["fb_app_secret"]
 end
